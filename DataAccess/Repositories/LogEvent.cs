5f4c45ff368208f4db01c662fd5777455a8e5ef4
Efitec <walter.atuncar@efitec-corp.com>
2023-04-09 20:17:05 -0500

    Update LogEvent.cs

--- DataAccess/Repositories/LogEvent.cs
+++ DataAccess/Repositories/LogEvent.cs
@@ -1,50 +1,88 @@
 ﻿using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
 using N5PermisosAPI.DataAccess.Interfaces;
 using Nest;
+using System.Net;
 using System.Text.Json;
 
 namespace N5PermisosAPI.DataAccess.Repositories
 {
     public class LogEvent : ILogEvent
     {
+        private readonly IConfiguration Configuration;
         private readonly IElasticClient _elasticClient;
         private readonly ProducerConfig _producerConfig;
-        public LogEvent(IElasticClient elasticClient, ProducerConfig producerConfig)
+        public LogEvent(IConfiguration configuration, IElasticClient elasticClient, ProducerConfig producerConfig)
         {
+            Configuration = configuration;
             _elasticClient = elasticClient;
             _producerConfig = producerConfig;
         }
         async Task ILogEvent.LogEventToElasticsearchAsync(string eventType, object eventData)
         {
-            var eventLog = new
+            try
             {
-                EventType = eventType,
-                EventData = eventData,
-                Timestamp = DateTime.UtcNow
-            };
+                using (var httpClient = new HttpClient())
+                {
+                    var response = await httpClient.GetAsync(Configuration["Elasticsearch:Uri"]);
 
-            await _elasticClient.IndexDocumentAsync(eventLog);
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        var eventLog = new
+                        {
+                            EventType = eventType,
+                            EventData = eventData,
+                            Timestamp = DateTime.UtcNow
+                        };
+
+                        await _elasticClient.IndexDocumentAsync(eventLog);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                
+            }
+           
         }
 
         async Task ILogEvent.LogEventToKafkaAsync(string eventType, object eventData)
         {
-            using (var producer = new ProducerBuilder<Null, string>(_producerConfig).Build())
+            try
             {
-                var eventLog = new
+                using (var httpClient = new HttpClient())
                 {
-                    Id = Guid.NewGuid(),
-                    OperationName = eventType,
-                    EventData = eventData,
-                    Timestamp = DateTime.UtcNow
-                };
+                    var response = await httpClient.GetAsync(Configuration["Kafka:BootstrapServers"]);
 
-                var message = new Message<Null, string>
-                {
-                    Value = JsonSerializer.Serialize(eventLog)
-                };
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        using (var producer = new ProducerBuilder<Null, string>(_producerConfig).Build())
+                        {
+                            var eventLog = new
+                            {
+                                Id = Guid.NewGuid(),
+                                OperationName = eventType,
+                                EventData = eventData,
+                                Timestamp = DateTime.UtcNow
+                            };
 
-                await producer.ProduceAsync(eventType, message);
+                            var message = new Message<Null, string>
+                            {
+                                Value = JsonSerializer.Serialize(eventLog)
+                            };
+
+                            await producer.ProduceAsync(eventType, message);
+                        }
+                    }
+                }
             }
+            catch (Exception)
+            {
+
+                
+            }
+          
         }
     }
 }
